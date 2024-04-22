5b0f12050c1a2eb555f2328061e0a941c927cce2
b23danhe <b23danhe@student.his.se>
2024-04-22 13:36:19 +0200

    Adding RecyclerViewAdapter.java class and RecyclerViewItem.java and some layout work.

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -11,12 +11,17 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
     private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
     private final String JSON_FILE = "mountains.json";
 
+    private RecyclerViewAdapter adapter;
+
+   // private Gson = gson;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new JsonFile(this, this).execute(JSON_FILE);
+        new JsonFile(this, this).execute(JSON_FILE); //Hämtar en lokal JSON textfil
+        new JsonTask(this).execute(JSON_URL); //Hämtar en JSON textfil från en URL
     }
 
     @Override
---
+++ app/src/main/java/com/example/networking/RecyclerViewAdapter.java
@@ -0,0 +1,5 @@
+package com.example.networking;
+
+public class RecyclerViewAdapter {
+
+}
